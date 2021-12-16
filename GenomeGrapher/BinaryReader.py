40171a5c76fb65a408157b26edc37d5d5c43a040
MacDonaldSchoolGit <74308087+AwesomePantsAP@users.noreply.github.com>
2021-12-16 22:35:30 +0000

    Update BinaryReader.py

--- GenomeGrapher/BinaryReader.py
+++ GenomeGrapher/BinaryReader.py
@@ -4,22 +4,55 @@ BinaryReader
 Reads a file and extracts sections of bits
 at a time.
 """
+import math
 
 class BinaryReader:
     #open the file at path and prepare to extract bits
     def __init__(self, path):
         #open and store the file at path
-        self.file = open(path)
+        self.file = open(path, "rb")
 
         #initialize variables
-        self.current_byte = self.file.read(1)
-        self.bits_left = 8
+        self.current_bits = []
+        self.bits_left = 0
 
     #reads n_bite bits from the open file
     def read(self, n_bits=1):
-        pass
+        #calculate how many more bits we need to read in
+        bytes_required = math.ceil((n_bits - self.bits_left) / 8)
+
+        #read the bits of the bytes into the current_bytes list
+        for i in range(bytes_required):
+            as_byte = self.file.read(1) #read in a byte
+            as_int = int.from_bytes(as_byte, byteorder="big") #cast it to an int
+            for i in range(8):
+                self.current_bits.append((as_int >> (7 - i)) & 0b1) #append each bit to the bit array
+
+        #reassemble and remove the bits requested, leaving the excess to be reused for the next call
+        final_result = 0
+        for i in range(n_bits):
+            final_result += self.current_bits.pop(0) #add the current bit at the 1's place
+            final_result << 1 #bit shift by 1 to make room for the next bit
+
+        #return the final value
+        return final_result
 
     #close the file for this reader
     def close(self):
         #close the file
         self.file.close()
+
+if __name__ == "__main__":
+    br = BinaryReader("../Genomes/test_genome_smallMOD.genome")
+
+    print(br.read(1))
+    print(br.read(15))
+
+    print(br.read(1))
+    print(br.read(1))
+    print(br.read(1))
+    print(br.read(10))
+    print(br.read(10))
+    print(br.read(9))
+
+    br.close()
