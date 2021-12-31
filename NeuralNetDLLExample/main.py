de37803dc36975762d8ae42ca6a2e2ad08f5f408
MacDonaldSchoolGit <74308087+AwesomePantsAP@users.noreply.github.com>
2021-12-31 16:43:06 +0000

    Added more testing to main.py

--- NeuralNetDLLExample/main.py
+++ NeuralNetDLLExample/main.py
@@ -58,18 +58,20 @@ if __name__ == "__main__":
     node0_conn_count = __get_node_connection_count(handle, ctypes.c_int(0))
     node1_conn_count = __get_node_connection_count(handle, ctypes.c_int(1))
 
-    node0_bias = __get_node_bias(handle, ctypes.c_int(0))
-
-    node1_conn0_weight = __get_connection_weight(handle, ctypes.c_int(1), ctypes.c_int(0))
-
     input_count = __get_network_input_count(handle)
     node_count = __get_network_node_count(handle)
     output_count = __get_network_output_count(handle)
 
     print(f"\n{input_count = }\n{node_count = }\n{output_count = }\n")
     print(f"{node0_conn_count = }\n{node1_conn_count = }\n")
-    print(f"{node0_bias = }")
-    print(f"{node1_conn0_weight = }")
+
+    for i in range(node_count):
+        for j in range(__get_node_connection_count(handle, ctypes.c_int(i))):
+            print(f"node{i}_conn{j}_weight = {__get_connection_weight(handle, ctypes.c_int(i), ctypes.c_int(j))}")
+    
+    for i in range(node_count, node_count + output_count):
+        for j in range(__get_node_connection_count(handle, ctypes.c_int(i))):
+            print(f"node{i}_conn{j}_weight = {__get_connection_weight(handle, ctypes.c_int(i), ctypes.c_int(j))}")
 
     inputs = [
         (ctypes.c_float * 2)(0, 0),
@@ -116,18 +118,20 @@ if __name__ == "__main__":
     input_count = __get_network_input_count(handle)
     node_count = __get_network_node_count(handle)
     output_count = __get_network_output_count(handle)
-
-    node0_bias = __get_node_bias(handle, ctypes.c_int(0))
-    
-    node1_conn0_weight = __get_connection_weight(handle, ctypes.c_int(1), ctypes.c_int(0))
     
     node0_conn_count = __get_node_connection_count(handle, ctypes.c_int(0))
     node1_conn_count = __get_node_connection_count(handle, ctypes.c_int(1))
 
     print(f"\n{input_count = }\n{node_count = }\n{output_count = }\n")
     print(f"{node0_conn_count = }\n{node1_conn_count = }\n")
-    print(f"{node0_bias = }")
-    print(f"{node1_conn0_weight = }")
+    
+    for i in range(node_count):
+        for j in range(__get_node_connection_count(handle, ctypes.c_int(i))):
+            print(f"node{i}_conn{j}_weight = {__get_connection_weight(handle, ctypes.c_int(i), ctypes.c_int(j))}")
+    
+    for i in range(node_count, node_count + output_count):
+        for j in range(__get_node_connection_count(handle, ctypes.c_int(i))):
+            print(f"node{i}_conn{j}_weight = {__get_connection_weight(handle, ctypes.c_int(i), ctypes.c_int(j))}")
 
     #test input/output again with different structure
     for input_set in inputs:
@@ -149,3 +153,48 @@ if __name__ == "__main__":
 
     #destroy the network now that we're done with it
     __destroy_network(handle)
+
+
+    #recreate the network
+    handle = ctypes.c_uint64(__create_network(
+        ctypes.c_char_p(b"../NeuralNetwork/Genomes/altered_genome.genome"), #genome path
+        ctypes.c_int(2), #inputs
+        ctypes.c_int(1), #outputs
+        ctypes.c_int(3), #activation function index
+        ctypes.c_bool(False) #verbose?
+    ))
+
+    #get network information
+    node0_conn_count = __get_node_connection_count(handle, ctypes.c_int(0))
+    node1_conn_count = __get_node_connection_count(handle, ctypes.c_int(1))
+
+    input_count = __get_network_input_count(handle)
+    node_count = __get_network_node_count(handle)
+    output_count = __get_network_output_count(handle)
+
+    print(f"\n{input_count = }\n{node_count = }\n{output_count = }\n")
+    print(f"{node0_conn_count = }\n{node1_conn_count = }\n")
+
+    for i in range(node_count):
+        for j in range(__get_node_connection_count(handle, ctypes.c_int(i))):
+            print(f"node{i}_conn{j}_weight = {__get_connection_weight(handle, ctypes.c_int(i), ctypes.c_int(j))}")
+    
+    for i in range(node_count, node_count + output_count):
+        for j in range(__get_node_connection_count(handle, ctypes.c_int(i))):
+            print(f"node{i}_conn{j}_weight = {__get_connection_weight(handle, ctypes.c_int(i), ctypes.c_int(j))}")
+
+    #test input/output
+    outputs = (ctypes.c_float*1)()
+    for input_set in inputs:
+        __set_network_inputs(
+            handle, #network handle
+            input_set, #inputs
+            ctypes.c_int(2) #number of inputs
+        )
+
+        __get_network_outputs(
+            handle, #network handle
+            outputs, #output array
+            ctypes.c_int(1) #number of outputs
+        )
+        print(f"{outputs[0] = }\n")
