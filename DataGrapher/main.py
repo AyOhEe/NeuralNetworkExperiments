ba525a5691111252c4a3b758a823b0899b22b508
MacDonaldSchoolGit <74308087+AwesomePantsAP@users.noreply.github.com>
2022-02-01 17:39:40 +0000

    Improved plot_data function

--- DataGrapher/main.py
+++ DataGrapher/main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import json
 
 #reads in data from a json file and graphs it using matplotlib
-def plot_data(data, plot):
+def plot_data(data):
     #if there is a title for the graph, use it
     if "Title" in data:
         plt.title(data["Title"])
@@ -11,21 +11,22 @@ def plot_data(data, plot):
     if "Labels" in data:
         if len(data["Labels"]) == 2:
             plt.xlabel(data["Labels"][0])
-            plt.xlabel(data["Labels"][1])
+            plt.ylabel(data["Labels"][1])
+
+    #if there is valid limit data, use it
+    if "xlim start" in data and "xlim end" in data and "ylim start" in data and "ylim end" in data:
+        plt.xlim(data["xlim start"], data["xlim end"])
+        plt.ylim(data["ylim start"], data["ylim end"])
 
     #for each of the lines in the data, plot the line
     if "Lines" in data:
         for line in data["Lines"]:
             # plot the points
             plt.plot(
-                data["Lines"]["DataY"], 
-                data["Lines"]["DataY"], 
-                color='green', 
-                linestyle='dashed', 
-                linewidth = 3, 
-                marker='o', 
-                markerfacecolor='blue', 
-                markersize=12)
+                line["DataX"], 
+                line["DataY"], 
+                label=line["Label"],
+                **line["kwargs"])
 
 if __name__ == "__main__":
     data = json.loads(open(input("File path: ")).read())
