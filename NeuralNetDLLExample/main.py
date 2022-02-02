334d46a4b6e8fd4396c8796ae6aeae29341b5a87
MacDonaldSchoolGit <74308087+AwesomePantsAP@users.noreply.github.com>
2022-02-02 07:47:18 +0000

    Performance changes in NeuralNetDLLExample main.py

--- NeuralNetDLLExample/main.py
+++ NeuralNetDLLExample/main.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     episode_length = 5000 # the maximum length of an episode
     generation_length = 200 # the amount of generations to run
     graph_frequency = 1 # how often a graph is made
+    display_frequency = 1 # how often the best network of a generation is displayed
 
     node_add_chance = 100 # x/1000 chance that a node is added
     connection_add_chance = 100 # x/1000 chance that a connection is added
@@ -16,6 +17,8 @@ if __name__ == "__main__":
 
     bias_alter_chance = 100 # x/1000 chance that a bias is altered
     weight_alter_chance = 100 # x/1000 chance that a weight is altered
+
+    environment = 'LunarLanderContinuous-v2' # the openai gym environment for the networks to train on
 
     #import openai-gym https://gym.openai.com/docs/
     import gym
@@ -40,7 +43,7 @@ if __name__ == "__main__":
         BLINK = '\033[m'
         
     #start the test environment
-    env = gym.make('LunarLanderContinuous-v2')
+    env = gym.make(environment)
     env.reset()
 
     #create the lines to store our statistics
@@ -104,17 +107,22 @@ if __name__ == "__main__":
         print(f"Generation finished with peak reward total {bcolors.FAIL}{scores[0][1]}{bcolors.ENDC} from network {bcolors.OKGREEN}{scores[0][0]}{bcolors.ENDC}")
 
         #test and display the best network
-        total_reward = 0
-        observation, reward, done, info = (0, 0), 0, False, {}
-        for t in range(episode_length):
-            env.render()
-            act = networks[scores[0][0]].CalculateOutputs(observation)
-            observation, reward, done, info = env.step(act) #do an action
-            total_reward += reward #store the reward for this iteration
+        if gen_i % display_frequency == 0:
+            total_reward = 0
+            observation, reward, done, info = (0, 0), 0, False, {}
+            for t in range(episode_length):
+                env.render()
+                act = networks[scores[0][0]].CalculateOutputs(observation)
+                observation, reward, done, info = env.step(act) #do an action
+                total_reward += reward #store the reward for this iteration
 
-            if done: #exit if we've finished the episode
-                break
-        env.reset()
+                if done: #exit if we've finished the episode
+                    break
+
+            #close the environment window for performance
+            env.close()
+            env = gym.make(environment)
+            env.reset()
 
         #destroy the bottom half
         for net_tuple in scores[len(scores)//2:]:
