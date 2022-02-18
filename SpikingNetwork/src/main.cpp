7b817bd9d0a9599e2416db0cc2ec6a15311d74cb
MacDonaldSchoolGit <74308087+AwesomePantsAP@users.noreply.github.com>
2022-02-18 22:42:30 +0000

    Brought in Eigen

--- SpikingNetwork/src/main.cpp
+++ SpikingNetwork/src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <Eigen/Dense>
 
-int main() 
+using Eigen::MatrixXd;
+
+int main()
 {
-	std::cout << "Hello, World!" << std::endl;
-
-	return 0;
+	MatrixXd m(2, 2);
+	m(0, 0) = 3;
+	m(1, 0) = 2.5;
+	m(0, 1) = -1;
+	m(1, 1) = m(1, 0) + m(0, 1);
+	std::cout << m << std::endl;
 }
