4d3778c5999ccf0b792b6edb88f1f49c5b3d4446
MacDonaldSchoolGit <74308087+AwesomePantsAP@users.noreply.github.com>
2022-02-26 22:52:51 +0000

    Added Gene constructors

--- SpikingNetwork/Network Manager/Assets/Scripts/ConnectionGene.cs
+++ SpikingNetwork/Network Manager/Assets/Scripts/ConnectionGene.cs
@@ -1,11 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
+
+[Serializable]
 public struct ConnectionGene
 {
+    //gene values
     public bool SourceType;
     public bool TargetType;
     public uint SourceIndex;
     public uint TargetIndex;
-    public Half Weight;
+    public float Weight;
+
+    ConnectionGene(byte[] bytes) 
+    {
+        //get our values
+        SourceType = (bytes[0] & 0b10000000) == 0b10000000;
+        TargetType = (bytes[0] & 0b01000000) == 0b01000000;
+        SourceIndex = BitConverter.ToUInt32(bytes, 1);
+        TargetIndex = BitConverter.ToUInt32(bytes, 5);
+        Weight = BitConverter.ToSingle(bytes, 9);
+    }
 }
--- SpikingNetwork/Network Manager/Assets/Scripts/NeuronGene.cs
+++ SpikingNetwork/Network Manager/Assets/Scripts/NeuronGene.cs
@@ -1,11 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 
+[Serializable]
 public struct NeuronGene
 {
+    //gene values
     public bool Type;
     public bool OutputType;
     public float MembraneResistance;
     public float ThresholdOffset;
+
+    //creates a Neuron gene from a byte list
+    NeuronGene(byte[] bytes)
+    {
+        //get our values
+        Type = (bytes[0] & 0b10000000) == 0b10000000;
+        OutputType = (bytes[0] & 0b01000000) == 0b01000000;
+        MembraneResistance = BitConverter.ToSingle(bytes, 1);
+        ThresholdOffset = BitConverter.ToSingle(bytes, 5);
+    }
 }
