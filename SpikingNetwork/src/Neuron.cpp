541a90e52125b88c83019fa5a6f5fbbd7b289b82
MacDonaldSchoolGit <74308087+AwesomePantsAP@users.noreply.github.com>
2022-03-05 13:11:22 +0000

    Defined Neuron constructor

---
+++ SpikingNetwork/src/Neuron.cpp
@@ -0,0 +1,15 @@
+#include "../Include/Neuron.h"
+
+//creates a neuron from a byte sequence
+Neuron::Neuron(unsigned char* bytes) 
+{
+	//calculate the membrane resistance and threshold offset
+	unsigned int IntMembraneResistance = (bytes[1] << 24) + (bytes[2] << 16) + (bytes[3] << 8) + bytes[4];
+	unsigned int IntThresholdOffset = (bytes[5] << 24) + (bytes[6] << 16) + (bytes[7] << 8) + bytes[8];
+
+	//get our values
+	NeuronType = (bytes[0] & 0b10000000) == 0b10000000;
+	OutputType = (bytes[0] & 0b01000000) == 0b01000000;
+	MembraneResistance = *((float*)&IntMembraneResistance);
+	ThresholdOffset = *((float*)&IntThresholdOffset);
+}
